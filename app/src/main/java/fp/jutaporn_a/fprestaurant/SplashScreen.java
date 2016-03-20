873e54af6ab93cb0186cf3d0f6472173c612a279
jutaporn <sassygirl_jr@hotmail.com>
2016-03-20 15:07:36 +0700

    sound finish 20 06 2559

---
+++ app/src/main/java/fp/jutaporn_a/fprestaurant/SplashScreen.java
@@ -0,0 +1,27 @@
+package fp.jutaporn_a.fprestaurant;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+        }, 7000);
+        MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.intro_tata);
+        mediaPlayer.start();
+    } //main method
+}//main class

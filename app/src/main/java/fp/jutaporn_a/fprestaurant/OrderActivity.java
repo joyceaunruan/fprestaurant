4bde30423fcbe3502e5ba6aad53a5ad8b2c5d940
jutaporn <sassygirl_jr@hotmail.com>
2016-03-20 14:48:10 +0700

    update ทั้งหมดได้แล้ว

--- app/src/main/java/fp/jutaporn_a/fprestaurant/OrderActivity.java
+++ app/src/main/java/fp/jutaporn_a/fprestaurant/OrderActivity.java
@@ -1,7 +1,13 @@
 package fp.jutaporn_a.fprestaurant;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.os.Build;
+import android.os.StrictMode;
+import android.support.v4.media.MediaDescriptionCompatApi21;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +16,20 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.ArrayList;
 
 public class OrderActivity extends AppCompatActivity {
     //Explicit
@@ -17,7 +37,12 @@ public class OrderActivity extends AppCompatActivity {
     private Spinner deskSpinner;
     private ListView foodListView;
 
-    private String officerString , deskString , foodString , amountString;
+    private String officerString, deskString, foodString, amountString;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
 
     @Override
@@ -33,6 +58,9 @@ public class OrderActivity extends AppCompatActivity {
 
         //create Food List view
         createFoodListview();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }   //Main Method
 
     private void createFoodListview() {
@@ -44,7 +72,7 @@ public class OrderActivity extends AppCompatActivity {
         int intcount = cursor.getCount();
 
         String[] iconStrings = new String[intcount];
-        String[] foodStrings = new String[intcount];
+        final String[] foodStrings = new String[intcount];
         String[] priceStrings = new String[intcount];
 
         for (int i = 0; i < intcount; i++) {
@@ -58,15 +86,99 @@ public class OrderActivity extends AppCompatActivity {
         cursor.close();
 
         FoodAdapter foodAdapter = new FoodAdapter(OrderActivity.this, iconStrings,
-                                                   foodStrings, priceStrings);
+                foodStrings, priceStrings);
         foodListView.setAdapter(foodAdapter);
+
+        foodListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                foodString = foodStrings[i];
+
+                chooseAmount();
+
+            }   //on Item Click
+        });
+
     }  //food list view
 
+    private void chooseAmount() {
+        CharSequence[] charSequences = {"1 จาน","2 จาน","3 จาน","4 จาน","5 จาน",};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(foodString);
+        builder.setSingleChoiceItems(charSequences, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                amountString = Integer.toString(i + 1);
+                dialogInterface.dismiss();
+
+                confirmOrder();
+
+
+            }   // onclick
+        });
+        builder.show();
+    }
+
+    private void confirmOrder() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setIcon(R.drawable.icon_myaccount);
+        builder.setTitle("โปรดตรวจทาน");
+        builder.setMessage("Officer =" + officerString + "\n" +
+                "Desk =" + deskString + "\n" +
+                "Food = " + foodString + "\n" +
+                "Amount =" + amountString);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                updateToServer();
+                dialogInterface.dismiss();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+
+            }
+        });
+        builder.show();
+
+    }   //confirm order
+
+    private void updateToServer() {
+        StrictMode.ThreadPolicy threadPolicy = new StrictMode.ThreadPolicy
+                .Builder().permitAll().build();
+        StrictMode.setThreadPolicy(threadPolicy);
+        try {
+            ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+            nameValuePairs.add(new BasicNameValuePair("isAdd","true"));
+            nameValuePairs.add(new BasicNameValuePair("Officer", officerString));
+            nameValuePairs.add(new BasicNameValuePair("Desk", deskString));
+            nameValuePairs.add(new BasicNameValuePair("Food", foodString));
+            nameValuePairs.add(new BasicNameValuePair("Amount", amountString));
+
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost("http://swiftcodingthai.com/19Mar/php_add_order.php");
+            httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs,"UTF-8"));
+            httpClient.execute(httpPost);
+
+            Toast.makeText(OrderActivity.this, "Order" + foodString + "เรียบร้อยแล้ว", Toast.LENGTH_SHORT).show();
+
+        } catch (Exception e) {
+            Toast.makeText(OrderActivity.this, "ไม่สามารถเชื่อมต่อได้", Toast.LENGTH_SHORT).show();
+
+        }
+
+    }   // Update to server
+
     private void createDeskSpinner() {
-        final String[] deskStrings = {"โต๊ะ 1","โต๊ะ 2","โต๊ะ 3","โต๊ะ 4","โต๊ะ 5","โต๊ะ 6","โต๊ะ 7","โต๊ะ 8","โต๊ะ 9","โต๊ะ 10"};
+        final String[] deskStrings = {"โต๊ะ 1", "โต๊ะ 2", "โต๊ะ 3", "โต๊ะ 4", "โต๊ะ 5", "โต๊ะ 6", "โต๊ะ 7", "โต๊ะ 8", "โต๊ะ 9", "โต๊ะ 10"};
 
         ArrayAdapter<String> stringArrayAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1,deskStrings);
+                android.R.layout.simple_list_item_1, deskStrings);
         deskSpinner.setAdapter(stringArrayAdapter);
 
         deskSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -95,4 +207,44 @@ public class OrderActivity extends AppCompatActivity {
         deskSpinner = (Spinner) findViewById(R.id.spinner);
         foodListView = (ListView) findViewById(R.id.listView);
     }
+
+   /* @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Order Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://fp.jutaporn_a.fprestaurant/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }*/
+
+   /* @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Order Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://fp.jutaporn_a.fprestaurant/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }*/
 }   // Main Class
